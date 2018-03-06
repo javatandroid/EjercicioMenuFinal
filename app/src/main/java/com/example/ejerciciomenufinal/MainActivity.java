577ce3c3a1d60840cb4785152444b25022210819
Javi <orte.mkt@gmail.com>
2018-03-06 13:27:16 +0100

    menu

--- app/src/main/java/com/example/ejerciciomenufinal/MainActivity.java
+++ app/src/main/java/com/example/ejerciciomenufinal/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.ejerciciomenufinal;
 
+import android.service.autofill.FillEventHistory;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,18 +19,25 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navView;
 
+
+    public MainActivity() {
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        appbar =(Toolbar)findViewById(R.id.appbar);
-        setSupportActionBar(appbar);
+
+
 
         getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_nav_menu);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+
+
+
         navView=(NavigationView)findViewById(R.id.navview);
 
         navView.setNavigationItemSelectedListener(
@@ -80,8 +91,24 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    private void setSupportActionBar(Toolbar appbar) {
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
+
+
 
 
 }
