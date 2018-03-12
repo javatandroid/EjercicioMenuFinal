619641722ff93b7216245bd6cb55ab0a6e1972e6
Javi <orte.mkt@gmail.com>
2018-03-12 13:35:28 +0100

    Algo suelto

--- app/src/main/java/com/example/ejerciciomenufinal/fragmentcategorias.java
+++ app/src/main/java/com/example/ejerciciomenufinal/fragmentcategorias.java
@@ -1,12 +1,15 @@
 package com.example.ejerciciomenufinal;
 
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by DIDACT on 05/03/2018.
@@ -19,6 +22,23 @@ public class fragmentcategorias extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+
+        //Icono lupa en AppBar
+        ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setDisplayOptions(actionBar.getDisplayOptions()
+                | ActionBar.DISPLAY_SHOW_CUSTOM);
+        ImageView imageView = new ImageView(actionBar.getThemedContext());
+        imageView.setScaleType(ImageView.ScaleType.CENTER);
+        imageView.setImageResource(R.mipmap.ic_lupa);
+        ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(
+                ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.WRAP_CONTENT, Gravity.RIGHT
+                | Gravity.CENTER_VERTICAL);
+        layoutParams.rightMargin = 40;
+        imageView.setLayoutParams(layoutParams);
+        actionBar.setCustomView(imageView);
+
         return inflater.inflate(R.layout.fragment_fragmentcategorias, container, false);
     }
 }
